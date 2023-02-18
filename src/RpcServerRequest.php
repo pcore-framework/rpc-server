bd199140416640426a539e64a9bd9258b67a394a
magomedcoder <info@magomedcoder.ru>
2023-02-19 00:29:05 +0300

    Added header and body bag

--- src/RpcServerRequest.php
+++ src/RpcServerRequest.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace PCore\RpcServer;
 
-use PCore\HttpMessage\Bags\ParameterBag;
+use PCore\HttpMessage\Bags\{HeaderBag, ParameterBag};
+use PCore\HttpMessage\Stream\StandardStream;
+use PCore\RpcServer\Bags\ServerBag;
+use PCore\RpcServer\Contracts\RpcServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
 
 /**
  * Class RpcServerRequest
  * @package PCore\RpcServer
  * @github https://github.com/pcore-framework/json-server
  */
-class RpcServerRequest
+class RpcServerRequest implements RpcServerRequestInterface
 {
 
     /**
@@ -24,10 +28,21 @@ class RpcServerRequest
      */
     protected ParameterBag $attributes;
 
-    public function __construct()
+    /**
+     * @var HeaderBag
+     */
+    protected HeaderBag $headers;
+
+    /**
+     * @var StreamInterface|null
+     */
+    protected ?StreamInterface $body = null;
+
+    public function __construct(array $headers = [])
     {
         $this->serverParams = new ServerBag();
         $this->attributes = new ParameterBag();
+        $this->headers = new HeaderBag($headers);
     }
 
     /**
@@ -35,13 +50,69 @@ class RpcServerRequest
      * @param array $attributes
      * @return static
      */
-    public static function createRequest($request, array $attributes = [])
+    public static function createRequest($request, array $attributes = []): RpcServerRequestInterface
     {
         $server = $request->server;
-        $psrRequest = new static();
+        $header = $request->header;
+        $psrRequest = new static($header);
         $psrRequest->serverParams = new ServerBag($server);
         $psrRequest->attributes = new ParameterBag($attributes);
+        $psrRequest->body = StandardStream::create((string)$request->getContent());
         return $psrRequest;
+    }
+
+    /**
+     * @return array
+     */
+    public function getServerParams(): array
+    {
+        return $this->serverParams->all();
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers->all();
+    }
+
+    /**
+     * @param $name
+     * @return bool|null
+     */
+    public function hasHeader($name): ?bool
+    {
+        return $this->headers->has($name);
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function getHeader($name): mixed
+    {
+        return $this->headers->get($name);
+    }
+
+    /**
+     * @param $name
+     * @return string
+     */
+    public function getHeaderLine($name): string
+    {
+        if ($this->hasHeader($name)) {
+            return implode(', ', $this->getHeader($name));
+        }
+        return '';
+    }
+
+    /**
+     * @return StreamInterface|null
+     */
+    public function getBody(): ?StreamInterface
+    {
+        return $this->body;
     }
 
 }
